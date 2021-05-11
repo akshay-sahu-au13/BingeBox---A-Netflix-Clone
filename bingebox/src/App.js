10f3e92ff7f28eb6097fe944ff16d896bd6760b7
Akshay Sahu <akki5233@gmail.com>
2021-05-12 02:22:34 +0530

    All the data id fetched and CSS is aprially done

--- bingebox/src/App.js
+++ bingebox/src/App.js
@@ -2,14 +2,22 @@
 import './App.css';
 import Row from './Row';
 import requests from './requests';
+import Banner from './Banner';
 
 function App() {
   return (
     <div className="App">
       <h1>Netflix clone</h1>
-      <Row title="BINGEBOX ORIGINALS" fetchUrl={requests.bingeBoxOriginals} />
+    <Banner />
+
+      <Row title="BINGEBOX ORIGINALS" fetchUrl={requests.bingeBoxOriginals} isLargeRow/>
       <Row title="Trending Now" fetchUrl={requests.trending} />
-      <Row title="Trending Now" fetchUrl={requests.trending} />
+      <Row title="Top Rated" fetchUrl={requests.topRated} />
+      <Row title="Action Movies" fetchUrl={requests.actionMovies} />
+      <Row title="Comedy Movies" fetchUrl={requests.comedyMovies} />
+      <Row title="Horror Movies" fetchUrl={requests.horrorMovies} />
+      <Row title="Romance Movies" fetchUrl={requests.romanceMovies} />
+      <Row title="Documentaries" fetchUrl={requests.documentaries} />
     </div>
   );
 }
