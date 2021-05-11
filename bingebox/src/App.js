ac8b214d53ceb626bf5c1d94cc371abe43d0e61b
Akshay Sahu <akki5233@gmail.com>
2021-05-12 01:35:36 +0530

    Pulled the initial data from movie API

--- bingebox/src/App.js
+++ bingebox/src/App.js
@@ -1,10 +1,15 @@
-// import logo from './logo.svg';
+
 import './App.css';
+import Row from './Row';
+import requests from './requests';
 
 function App() {
   return (
     <div className="App">
       <h1>Netflix clone</h1>
+      <Row title="BINGEBOX ORIGINALS" fetchUrl={requests.bingeBoxOriginals} />
+      <Row title="Trending Now" fetchUrl={requests.trending} />
+      <Row title="Trending Now" fetchUrl={requests.trending} />
     </div>
   );
 }
